8a4074cba294060e7fba5f8b099b12cf66c1d595
isaac545454 <isaac.newton1221@gmail.com>
2023-02-22 12:31:49 -0300

    chore: routes get

--- src/User/user.controller.ts
+++ src/User/user.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 
 @Controller("users")
 export class UserController {
   @Post()
   async create(@Body() Body) {
     return { Body };
+  }
+
+  @Get()
+  async read() {
+    return { users: [] };
+  }
+
+  @Get(":id")
+  async readOne(@Param() param) {
+    return { user: {}, param };
   }
 }
