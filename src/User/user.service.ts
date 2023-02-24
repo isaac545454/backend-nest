07ce90f0f6bbf106b6c0e54632e09b2c48acb011
isaac545454 <isaac.newton1221@gmail.com>
2023-02-24 19:56:45 -0300

    feat: delete

--- src/User/user.service.ts
+++ src/User/user.service.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from ".prisma/client";
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { CreateUserDTO } from "./dto/create-user.dto";
 
@@ -8,6 +8,7 @@ export class UserService {
   constructor(private readonly prisma: PrismaService) {
     PrismaClient;
   }
+
   async create({ email, name, password }: CreateUserDTO) {
     return this.prisma.user.create({
       data: { email, name, password },
@@ -24,5 +25,12 @@ export class UserService {
 
   async findOne(id: number) {
     return this.prisma.user.findUnique({ where: { id: id } });
+  }
+
+  async delete(id: number) {
+    if (!(await this.findOne(id))) {
+      throw new NotFoundException("o usuario não existe");
+    }
+    return this.prisma.user.delete({ where: { id: id }, select: { id: true } });
   }
 }
