ef4840b3c8d24c400c281d2a66bb376aabdcfcdd
isaac545454 <isaac.newton1221@gmail.com>
2023-03-02 20:13:34 -0300

    feat: add limit request

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,26 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { AuthModule } from "./auth/auth.module";
 import { UserModule } from "./User/user.module";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
+import { APP_GUARD } from "@nestjs/core";
 
 @Module({
-  imports: [forwardRef(() => UserModule), forwardRef(() => AuthModule)],
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 1000,
+    }),
+    forwardRef(() => UserModule),
+    forwardRef(() => AuthModule),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
   exports: [AppService],
 })
 export class AppModule {}
